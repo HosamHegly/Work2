a9f8a378b227d7c2f32614dabb2adf4c455e0e84
tank351 <hoossam_one@hotmail.com>
2019-11-23 11:07:45 +0200

    ‫‪ss_hw2‬‬

---
+++ myBank.h
@@ -0,0 +1,11 @@
+void Addaccount(double );
+void accountsum(int );
+void entermoney(double , int );
+void pullmoney(double , int );
+void closeaccount(int );
+void addrepeat(double );
+void prinaccounts();
+void close();
+
+
+
